9f248a0a532ff3bf3708d796d713ecd90d83eb99
M4UR7C3 <moritz.stemmer@fcn-cluberer.de>
2021-12-06 13:51:23 +0100

    1.0.2
    Commands ausgelagert in extensions Ordner

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import sys
+import traceback
+from os import listdir
+from os.path import isfile, join
+
 import discord
 from discord.ext import commands
 import pytz
@@ -61,28 +66,15 @@ class PythonBot(commands.Bot):
 
 bot = PythonBot(intents=discord.Intents.all(), command_prefix='!')
 
-
-@bot.command(name='test')
-async def test(ctx, *args):
-    await ctx.send(f'Eingabe: {" ".join(args)}')
-
-
-@bot.command(name='userinfo')
-async def test(ctx, member: discord.Member):
-    de = pytz.timezone('Europe/Berlin')
-    embed = discord.Embed(title=f'> Userinfo für {member.display_name}',
-                          description='', color=0x4cd137, timestamp=datetime.now().astimezone(tz=de))
-    embed.add_field(name='Name', value=f'```{member.name}#{member.discriminator}```', inline=True)
-    embed.add_field(name='Bot', value=f'```{"Ja" if member.bot else "Nein"}```', inline=True)
-    embed.add_field(name='Nickname', value=f'```{(member.nick if member.nick else "Nicht gesetzt")}```', inline=True)
-    embed.add_field(name='Server beigetreten', value=f'```{member.joined_at}```', inline=True)
-    embed.add_field(name='Discord beigetreten', value=f'```{member.created_at}```', inline=True)
-    embed.add_field(name='Rollen', value=f'```{len(member.roles) - 1}```', inline=True)
-    embed.add_field(name='Höchste Rolle', value=f'```{member.top_role.name}```', inline=True)
-    embed.add_field(name='Farbe', value=f'```{member.color}```', inline=True)
-    embed.add_field(name='Booster', value=f'```{("Ja" if member.premium_since else "Nein")}```', inline=True)
-    embed.set_footer(text=f'Angefordert von {ctx.author.name} ~ {ctx.author.id}', icon_url=ctx.author.avatar_url)
-    await ctx.send(embed=embed)
+if __name__ == '__main__':
+    path = 'extensions'
+    extensions = [file for file in listdir(path) if isfile(join(path, file))]
+    for extension in extensions:
+        try:
+            bot.load_extension(f'{path}.{extension[:-3]}')
+        except Exception as exception:
+            print(f'Failed to load extension {extension}.', file=sys.stderr)
+            traceback.print_exc()
 
 
 bot.run('Token')
